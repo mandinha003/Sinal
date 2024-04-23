0818d7cc6eb003be063a2c80b1fd9c22878588c2
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-23 20:22:17 -0300

    terminei

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿double numero;
+
+Console.Write("Digite um número: ");
+numero = Convert.ToDouble(Console.ReadLine());
+
+if (numero > 0)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("POSITIVO");
+    Console.ResetColor();
+}
+else
+{
+    if (numero < 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("NEGATIVO");
+        Console.ResetColor();
+    }
+    else
+    {
+        Console.WriteLine("Zero");
+    }
+
+}
